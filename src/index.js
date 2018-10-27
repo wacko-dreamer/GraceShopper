44a2d6107d88a8cfc82a3a51572be9f252bda1e2
amrthameen <thameen.amr@gmail.com>
2018-10-27 12:02:42 -0400

    Added components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+
+import React from 'react'
+import {render} from 'react-dom'
+import App from './components/App.js'
+
+const root = document.getElementById('root')
+
+render(<App/>, root)
